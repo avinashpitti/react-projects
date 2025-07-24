b9b52a652095c9b995c6f2794b1b1cb1acc11c2d
avinashpitti <avinash87129@gmail.com>
2025-07-24 22:28:47 +0530

    added an image

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,7 +1,8 @@
+import profile from './assets/avi.jpg'
 function Card(){
     return(
             <div className="card">
-                <img src="" alt="" />
+                <img src={profile} alt="image" width="150px" height="120px" />
                 <h2>Avinash</h2>
                 <p>A python developer</p>
 
