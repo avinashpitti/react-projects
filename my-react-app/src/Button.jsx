2bae7188faa27380abd6fb3a5133bfd4dfdadab8
avinashpitti <avinash87129@gmail.com>
2025-07-27 18:17:59 +0530

    onclick event

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,11 +1,18 @@
 function Button(){
-    const handleClick=()=>console.log("ouch!");
+    
+    let count=0;
+    
+    const handleClick=(name)=>{
+        if(count<3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        }
+        else{
+            console.log(`${name} stop clicking me!`);
+        }
+    };
 
-    const handleClick2=(name)=>console.log('${name} stop clicking me')
-
-    return(
-        <button onClick={()=>handleClick2("Bro")} className="avi">click me  </button>
-    );
+    return (<button onClick={()=>handleClick("Bro")}>Click me</button>);
 }
 
 export default Button
