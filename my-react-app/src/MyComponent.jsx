9c9fef1a6a2223daea2f3b170b1874c4eff11f3f
avinashpitti <avinash87129@gmail.com>
2025-07-28 12:54:44 +0530

    update objects in state

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,29 +1,32 @@
 import React,{useState} from "react"
 function MyComponent(){
 
-   const[count,setCount]=useState(0);
-
-   function increment(){
-        setCount(c=>c+1);
-        setCount(c=>c+1);
-        setCount(c=>c+1);
-   };
-
-   function decrement(){
-        setCount(count-1);
-   };
-
-   function reset(){
-        setCount(0);
-   }
+   const [car,setCar]=useState({year:2024,
+                                make:" Ford ",
+                                model:"mustang"});
+   
     
+    function handleYearChange(event){
+            setCar(c=>({...c,year:event.target.value}));
+    }
+
+    function handleMakeChange(event){
+            setCar(c=>({...c,make:event.target.value}));
+    }
+    
+
+    function handleModelChange(event){
+            setCar(c=>({...c,model:event.target.value}));
+    }
+    
+
 
     return(
         <div>
-            <p>Count: {count}</p>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={reset}>Reset</button>
-            <button onClick={increment}>Increment</button>
+            <p>Your favorite car is: {car.year}{car.make}{car.model}</p>
+            <input type="number" value={car.year} onChange={handleYearChange} /> <br />
+            <input type="text" value={car.make} onChange={handleMakeChange} /> <br />
+            <input type="text" value={car.model} onChange={handleModelChange} /> <br />
 
         </div>
     );
