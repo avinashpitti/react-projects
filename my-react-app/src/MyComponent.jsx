17f9d37b44204b5a1ca7cf68e2fa4861b32862d1
avinashpitti <avinash87129@gmail.com>
2025-07-29 07:49:31 +0530

    created a todolist app

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,33 +1,24 @@
 import React,{useState} from "react"
 function MyComponent(){
 
-   const [car,setCar]=useState({year:2024,
-                                make:" Ford ",
-                                model:"mustang"});
    
-    
-    function handleYearChange(event){
-            setCar(c=>({...c,year:event.target.value}));
+    const [foods,setFoods]=useState(["Apple","Orange","Banana"]);
+
+    function handleAddFood(){
+
     }
 
-    function handleMakeChange(event){
-            setCar(c=>({...c,make:event.target.value}));
+    function handleRemoveFood(){
+        
     }
     
-
-    function handleModelChange(event){
-            setCar(c=>({...c,model:event.target.value}));
-    }
-    
-
 
     return(
         <div>
-            <p>Your favorite car is: {car.year}{car.make}{car.model}</p>
-            <input type="number" value={car.year} onChange={handleYearChange} /> <br />
-            <input type="text" value={car.make} onChange={handleMakeChange} /> <br />
-            <input type="text" value={car.model} onChange={handleModelChange} /> <br />
-
+            <h2>List of foods</h2>
+            <ul>
+                {foods.map((food,index)=> <li></li>)}
+            </ul>
         </div>
     );
 
